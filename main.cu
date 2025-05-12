948f528185dee804d6b40f563680c394b5669856
kamei <leokamei1@gmail.com>
2025-05-12 19:27:39 -0300

    final commit!

--- main.cu
+++ main.cu
@@ -28,40 +28,34 @@ color random_color(double min, double max) {
 int main() {
     srand(time(0));
 
-    // Número estimado de esferas (ajustado conforme o grid)
     const int max_spheres = 500;
     sphere* spheres = (sphere*)malloc(sizeof(sphere) * max_spheres);
     int sphere_count = 0;
 
-    // Ground
     spheres[sphere_count++] = sphere(point3(0, -1000, 0), 1000, LAMBERTIAN, color(0.5, 0.5, 0.5), 0, 0);
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
-            if (sphere_count >= max_spheres - 3) break; // segurança para não ultrapassar o limite
+            if (sphere_count >= max_spheres - 3) break;
 
             double choose_mat = random_double();
             point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
             if ((center - point3(4, 0.2, 0)).length() > 0.9) {
                 if (choose_mat < 0.8) {
-                    // Diffuse
                     color albedo = random_color() * random_color();
                     spheres[sphere_count++] = sphere(center, 0.2, LAMBERTIAN, albedo, 0, 0);
                 } else if (choose_mat < 0.95) {
-                    // Metal
                     color albedo = random_color(0.5, 1.0);
                     double fuzz = random_double() * 0.5;
                     spheres[sphere_count++] = sphere(center, 0.2, METAL, albedo, fuzz, 0);
                 } else {
-                    // Glass
                     spheres[sphere_count++] = sphere(center, 0.2, DIELECTRIC, color(0, 0, 0), 0, 1.5);
                 }
             }
         }
     }
 
-    // Três esferas grandes
     spheres[sphere_count++] = sphere(point3(0, 1, 0), 1.0, DIELECTRIC, color(0, 0, 0), 0, 1.5);
     spheres[sphere_count++] = sphere(point3(-4, 1, 0), 1.0, LAMBERTIAN, color(0.4, 0.2, 0.1), 0, 0);
     spheres[sphere_count++] = sphere(point3(4, 1, 0), 1.0, METAL, color(0.7, 0.6, 0.5), 0.0, 0);
